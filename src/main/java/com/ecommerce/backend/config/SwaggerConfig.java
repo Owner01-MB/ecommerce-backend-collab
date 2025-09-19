e31f53c13544ae8e9f3cd96d370c5ee41cf69088
Mayur <mayur415508@gmail.com>
2025-09-19 16:26:48 +0530

    bypass swagger through jwt security

--- src/main/java/com/ecommerce/backend/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/backend/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.ecommerce.backend.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,10 +17,13 @@ public class SwaggerConfig {
   @Bean
   public OpenAPI customOpenAPI() {
     return new OpenAPI()
-        .info(new Info()
-            .title("Ecommerce Backend API")
-            .version("1.0")
-            .description("API documentation for the Ecommerce backend project (DEV only)"));
+        .components(new Components()
+            .addSecuritySchemes("bearerAuth",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
   }
 
 }
